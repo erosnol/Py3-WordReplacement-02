d8a1505ea628698594b6b81b7a8df145dec052b8
Eros Nolasco-Mendoza <erosnolasco@me.com>
2022-10-20 17:02:26 -0400

    second commit

--- word replacement.py
+++ word replacement.py
@@ -1,1 +1,9 @@
 
+def replace_word():
+
+    str = "Hi guys, this is Eros, and I and studying for Psychology"
+    word_to_replace = input("Enter word to Replace: ")
+    word_replacement = input("Enter new word: ")
+    print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
